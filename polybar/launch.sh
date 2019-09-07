e7108226221ce847fd28bbf63899950cd9bd4220
Moussa <moussa.fofana@epitech.eu>
2019-09-07 22:16:23 +0200

    changed bar && mifred to i3-gaps-rounded

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,6 +8,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar
-polybar -c ~/.config/polybar/config.ini top &
-polybar -c ~/.config/polybar/config.ini bottom &
+# Launch bar1 and bar2
+polybar -c ~/.config/polybar/config.ini main &
