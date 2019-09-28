4943fe9658a895fb53bb452b60b35d88f6de09ff
Moussa <moussa.fofana@epitech.eu>
2019-09-28 21:27:09 +0200

    fixed bar background

--- bar/launchbar.sh
+++ bar/launchbar.sh
@@ -4,7 +4,7 @@ killall -q lemonbar
 # Wait killing lemonbar instance
 while pgrep -u $UID -x lemonbar >/dev/null; do sleep 1; done
 
-# Define background foreground colo from Xresources
+# Define background foreground color from Xresources
 BG="$(xrdb -query | grep '*background'| awk '{print $NF}')"
 
 # If compton is running activate transparency
@@ -15,4 +15,4 @@ BG="$(xrdb -query | grep '*background'| awk '{print $NF}')"
 # fi
 
 # Finaly launching bar
-sh ~/Dotfiles/bar/bar.sh | lemonbar -g 1920x20+0+0 -f "Cascadia Code:size=10" | sh &
+sh ~/Dotfiles/bar/bar.sh | lemonbar -B "$BG" -g 1920x20+0+0 -f "Cascadia Code:size=10" | sh &
