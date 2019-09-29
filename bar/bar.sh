d87925a9701a584dd55183ac592f4095a5fe29ca
Moussa <moussa.fofana@epitech.eu>
2019-09-29 13:27:21 +0200

    improved blur script now run idependanlty of my personal config && update wallpaper itself

--- bar/bar.sh
+++ bar/bar.sh
@@ -1,13 +1,6 @@
 #! /usr/bin/zsh
 
-FG="$(xrdb -query | grep '*foreground'| awk '{print $NF}')"
-COLOR0="$(xrdb -query | grep '*color0'| awk '{print $NF}')"
-COLOR1="$(xrdb -query | grep '*color1'| awk '{print $NF}')"
-COLOR2="$(xrdb -query | grep '*color2'| awk '{print $NF}')"
-COLOR3="$(xrdb -query | grep '*color3'| awk '{print $NF}')"
-COLOR4="$(xrdb -query | grep '*color4'| awk '{print $NF}')"
-COLOR5="$(xrdb -query | grep '*color5'| awk '{print $NF}')"
-COLOR6="$(xrdb -query | grep '*color6'| awk '{print $NF}')"
+. ~/.cache/wal/colors.sh
 
 fnc_batteries() {
     # saving batteries percentages && status
@@ -63,9 +56,9 @@ fnc_time() {
 fnc_launcher() {
     if [ -z "$(pgrep rofi)" ]
         then
-            echo -n "%{A:rofi -show drun:}%{F$COLOR5}  Launch%{A}%{F-}"
+            echo -n "%{A:rofi -show drun:}%{F$color5}  Launch%{A}%{F-}"
         else
-            echo -n "%{A:rofi -show drun:}%{F$COLOR2}  close %{A}%{F-}"
+            echo -n "%{A:rofi -show drun:}%{F$color2}  close %{A}%{F-}"
     fi
 }
 
@@ -76,15 +69,15 @@ while :; do
 
     # Center
     BAR="${BAR}%{c}"
-    BAR="${BAR}[%{F$COLOR3}$(fnc_workspace)%{F-}]"
-    BAR="${BAR} %{F$COLOR4}DATE:%{F-} $(fnc_date)"
-    BAR="${BAR} %{F$COLOR5}TIME:%{F-} $(fnc_time)"
-    BAR="${BAR} %{F$COLOR6}VOL:%{F-} $(fnc_sound)"
-    BAR="${BAR} %{F$COLOR2}BAT:%{F-} $(fnc_batteries)"
+    BAR="${BAR}[%{F$color3}$(fnc_workspace)%{F-}]"
+    BAR="${BAR} %{F$color4}DATE:%{F-} $(fnc_date)"
+    BAR="${BAR} %{F$color5}TIME:%{F-} $(fnc_time)"
+    BAR="${BAR} %{F$color6}VOL:%{F-} $(fnc_sound)"
+    BAR="${BAR} %{F$color2}BAT:%{F-} $(fnc_batteries)"
 
     # Right
     BAR="${BAR}%{r}"
-    BAR="${BAR}%{A:shutdown 0:} %{F$COLOR5}Shutdown%{O10}%{A}%{F-}"
+    BAR="${BAR}%{A:shutdown 0:} %{F$color5}Shutdown%{O10}%{A}%{F-}"
     
     # Output result
     echo $BAR
