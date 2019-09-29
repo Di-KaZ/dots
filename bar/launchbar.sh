88a663d28073c04cd71ea89adafb40b21c0dd540
Moussa <moussa.fofana@epitech.eu>
2019-09-29 16:31:17 +0200

    cleaned conf && tweaked blur script [error on long term running]#

--- bar/launchbar.sh
+++ bar/launchbar.sh
@@ -1,18 +1,11 @@
 #! /usr/bin/zsh
 killall -q lemonbar
 
+# for background color
+source ~/.cache/wal/colors.sh
+
 # Wait killing lemonbar instance
 while pgrep -u $UID -x lemonbar >/dev/null; do sleep 1; done
 
-# Define background foreground color from Xresources
-BG="$(xrdb -query | grep '*background'| awk '{print $NF}')"
-
-# If compton is running activate transparency
-# if [ -n "$(pgrep compton)" ]
-#     then
-#         BG="$(echo "$BG" | head -n1 | awk '{print $1;}')"
-#         BG="$(echo ${BG:0:1}80${BG:1:6})"
-# fi
-
-# Finaly launching bar
-sh ~/Dotfiles/bar/bar.sh | lemonbar -B "$BG" -g 1920x20+0+0 -f "Cascadia Code:size=10" | sh &
+# launch it
+sh ~/Dotfiles/bar/bar.sh | lemonbar -B "$background" -g 1920x20+0+0 -f "Cascadia Code:size=10" | sh &
