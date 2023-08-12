19270cea93e2bc7d2b73a079d84df213a8feb109
GET_MOUSSED <m.fofana@come-con.co>
2023-08-12 03:05:05 +0200

    feat more nvim config & keybinds

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,7 +1,7 @@
 -- LSP Zero
-local lsp = require('lsp-zero')
-
-lsp.preset("recommended")
+local cmp = require('cmp')
+local lsp = require('lsp-zero');
+lsp.preset({})
 
 lsp.ensure_installed({
     "tsserver",
@@ -32,8 +32,51 @@ require 'lspconfig'.lua_ls.setup {
     end
 }
 
-lsp.on_attach(function(_, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
+lsp.set_sign_icons({
+    error = '󰅙',
+    warn = '󰋼',
+    hint = '󰌵',
+    info = '󰋽'
+})
+
+lsp.on_attach(function(_, _)
+    -- lsp.default_keymaps({ buffer = bufnr, omit = {'gr', 'K'} })
+    lsp.buffer_autoformat()
+    local wk = require("which-key")
+
+    wk.register({
+        K = { vim.lsp.buf.hover, "Symbol information" },
+        ["<F2>"] = { vim.lsp.buf.rename, "Rename" },
+        g = {
+            r = { "<cmd> Telescope lsp_references <cr>", "Symbol references" },
+            i = { "<cmd> Telescope lsp_implementations <cr>", "Symbol implementation" },
+            d = { "<cmd> Telescope lsp_definitions <cr>", "Symbol definition" },
+            D = { vim.lsp.buf.declaration, "Symbol declaration" },
+            a = { vim.lsp.buf.code_action, "Symbol declaration" },
+            I = { "<cmd> Telescope diagnostics <cr>", "Diagnose Buffers" }
+        }
+    })
 end)
 
 lsp.setup()
+
+local cmp_action = lsp.cmp_action()
+cmp.setup({
+    completion = {
+        autocomplete = false
+    },
+    formatting = {
+        fields = { 'abbr', 'kind', 'menu' },
+        format = require('lspkind').cmp_format({
+            mode = 'symbol',       -- show only symbol annotations
+            maxwidth = 50,         -- prevent the popup from showing more than provided characters
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+        })
+    },
+    mapping = {
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }
+})
--- nvim/lua/map.lua
+++ nvim/lua/map.lua
@@ -5,15 +5,18 @@ wk.register({
     ["<leader>"] = {
         f = {
             name = "Files",
-            f = { "<cmd>Telescope find_files<cr>", "Find Files" }
+            f = { "<cmd>Telescope find_files<cr>", "Find Files" },
+            g = { "<cmd>Telescope live_grep<cr>", "Find grep" },
+            o = { "<cmd>Telescope buffers<cr>", "opened files" }
+
         },
         p = {
             v = { "<cmd> Ex <cr>", "File browser" }
         },
         t = {
             name = "Flutter",
-            d = {"<cmd> FlutterDevices <cr>", "Flutter: Show devices" },
-            c = {"<cmd> Telescope flutter commands <cr>", "Flutter: commands" }
+            d = { "<cmd> FlutterDevices <cr>", "Flutter: Show devices" },
+            c = { "<cmd> Telescope flutter commands <cr>", "Flutter: commands" }
         }
     }
 })
